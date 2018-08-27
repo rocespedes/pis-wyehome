87b33f84faa5a4741d4a53e7ab2cea5dc0e3658a
Joaquin Villar <joacovillar93@gmail.com>
2018-08-27 18:53:55 -0300

    delete google control

--- api/config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '352515521648-t3u90lo3i74tfoe5jti8jp9h31pmsdhc.apps.googleusercontent.com', '9B6XYakAD84sHU8UIgt7yfkH', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-end
